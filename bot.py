2ae4ca2e80e5e435a1803eaca36ee46fc178cd35
Calvin <calvin.keats@gmail.com>
2021-06-27 22:15:19 -0500

    it wasn't actually broken?

--- bot.py
+++ bot.py
@@ -117,8 +117,6 @@ async def on_message(message):
                 headers = { 'Authorization' : 'Bearer {0}'.format(client.twitterToken) }
                 tweetResponse = requests.get(url, headers=headers)
                 tweetData = json.loads(tweetResponse.text)
-                print(tweetData)
-                return
 
                 url = 'https://api.twitter.com/2/users/{0}?user.fields=verified'.format(tweetData['data']['author_id'])
                 authorResponse = requests.get(url, headers=headers)
